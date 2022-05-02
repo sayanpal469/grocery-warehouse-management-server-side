30eeb77533f3596a46fe6b08d05fbf8e42a75893
sayanpal469 <sayanpal469@gmail.com>
2022-05-02 23:43:56 +0530

    databse connected

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39pjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect()
+        const productCollection = client.db('groceryWarehouse').collection('products')
+
+        app.get('/product', async (req, res) => {
+            const query = {}
+
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally{
+        
+    }
+}
+
+
+run().catch(console.dir)
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
