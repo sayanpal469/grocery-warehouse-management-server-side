b8ad0e11a9982b62fdc422f14a8cd2f848d04e32
sayanpal469 <sayanpal469@gmail.com>
2022-05-03 00:06:52 +0530

    get product id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // Middleware
@@ -24,6 +24,13 @@ async function run() {
             const cursor = productCollection.find(query)
             const products = await cursor.toArray()
             res.send(products)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const product = await productCollection.findOne(query)
+            res.send(product)
         })
     }
     finally{
