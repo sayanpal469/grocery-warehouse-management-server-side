caf6da1ae440da28265ea408eb1ab8c7ec4f3f48
sayanpal469 <sayanpal469@gmail.com>
2022-05-03 22:05:24 +0530

    quantity updated

--- index.js
+++ index.js
@@ -32,6 +32,38 @@ async function run() {
             const product = await productCollection.findOne(query)
             res.send(product)
         })
+
+        // Delever Quantity
+        app.put('/product/:id', async (req,res) => {
+          const id = req.params.id
+          const deleverQuantity = req.body
+          const filter = {_id: ObjectId(id)}
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: {
+              quantity: deleverQuantity.newQuantity
+            }
+          };
+          const result = await productCollection.updateOne(filter, updateDoc, options)
+          res.send(result)
+          })
+
+
+        // Update Quantity
+        app.put('/product/:id', async (req,res) => {
+          const id = req.params.id
+          const updateQuantity = req.body
+          const filter = {_id: ObjectId(id)}
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: {
+              quantity: updateQuantity.newQuantity
+            }
+          };
+          const result = await productCollection.updateOne(filter, updateDoc, options)
+          res.send(result)
+        })
+
     }
     finally{
         
